6b1330198e3633ffe5eba8dbc38959e18e7ad3ad
Giovanni Ruocco <giovanniruocco1@gmail.com>
2024-02-07 16:22:04 +0100

    about and contact splitted + CV timeline added

--- assets/javascript/send_mail.js
+++ assets/javascript/send_mail.js
@@ -10,7 +10,9 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     // For example, you can send an AJAX request or perform client-side validation
 });
 
-function validateForm() {
+
+
+/* function validateForm() {
     var userFirstName = document.getElementById("user_firstname").value;
     var userLastName = document.getElementById("user_lastname").value;
     var userEmail = document.getElementById("user_email").value;
@@ -28,7 +30,66 @@ function validateForm() {
         // Not all fields are filled, show an alert or perform other validation logic
         alert("Please fill in all required fields");
     }
+} */
+
+
+
+
+function sanitizeInput(input) {
+    // Replace HTML tags with empty strings
+    return input.replace(/<[^>]*>/g, '');
 }
+
+function validateForm() {
+    var name = sanitizeInput(document.getElementById("name").value);
+    var surname = sanitizeInput(document.getElementById("surname").value);
+    var email = sanitizeInput(document.getElementById("email").value);
+    var subject = sanitizeInput(document.getElementById("subject").value);
+    var message = sanitizeInput(document.getElementById("message").value);
+
+    // Simple checks for empty fields
+    if (name === "") {
+        document.getElementById("nameError").innerHTML = "Name must be filled out";
+        return false;
+    }
+
+    if (surname === "") {
+        document.getElementById("surnameError").innerHTML = "Surname must be filled out";
+        return false;
+    }
+
+    if (email === "") {
+        document.getElementById("emailError").innerHTML = "Email must be filled out";
+        return false;
+    }
+
+    if (subject === "") {
+        document.getElementById("subjectError").innerHTML = "Subject must be filled out";
+        return false;
+    }
+
+    if (message === "") {
+        document.getElementById("messageError").innerHTML = "Message must be filled out";
+        return false;
+    }
+
+    // Additional checks for email format
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        document.getElementById("emailError").innerHTML = "Invalid email format";
+        return false;
+    }
+
+    // You can add more checks for other fields if needed
+
+    // If all checks pass, the form is valid
+    return true;
+}
+
+
+
+
+
 
 function sendMail(){
     var params = {
