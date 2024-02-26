c4835340b7e9b8669b7d0dc77a8666457c8918f4
Giovanni Ruocco <giovanniruocco1@gmail.com>
2024-02-26 16:44:53 +0100

    form validation message

--- assets/javascript/send_mail.js
+++ assets/javascript/send_mail.js
@@ -10,10 +10,14 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     // For example, you can send an AJAX request or perform client-side validation
 });
 
+document.getElementById('contact-form').setAttribute('autocomplete', 'off');
+
 
 function sendMail(){
 
     scrollToMessageArea();
+
+    var input_error = document.querySelector(".form_input_error");
 
     var params = {
         from_name: document.getElementById("form_name").value + " " + document.getElementById("form_lastname").value,
@@ -24,9 +28,14 @@ function sendMail(){
 
     if (validateForm()){
         emailjs.send('service_oy42a15', 'template_xdjh6jr', params).then(function () {
-            console.log('SUCCESS!');
+            input_error.innerHTML = "Message sent!"
+            input_error.classList.add("form_input_error_visible");
+            input_error.classList.add("success");
         }, function (error) {
-            console.log('FAILED...', error);
+            console.log(error);
+            input_error.classList.remove("success");
+            input_error.classList.add("form_input_error_visible");
+            input_error.innerHTML = "There was an error while sending your message!\nPlease try again."
         });
     }
 }
@@ -57,34 +66,35 @@ function validateForm() {
     var email = sanitizeInput(document.getElementById("form_email").value);
     var subject = sanitizeInput(document.getElementById("form_subject").value);
     var message = sanitizeInput(document.getElementById("form_message").value);
-    var input_error = document.getElementById("form_input_error");
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var input_error = document.querySelector(".form_input_error");
+    //var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
     // Simple checks for empty fields
     if (name === "") {
         input_error.innerHTML = "Name must be filled out";
-        return false;
     }
     
     else if (surname === "") {
         input_error.innerHTML = "Surname must be filled out";
-        return false;
     }
     else if (email === "") {
         input_error.innerHTML = "Email must be filled out";
-        return false;
     }
     else if (!emailRegex.test(email)) {
         input_error.innerHTML = "Invalid email format";
-        return false;
     }
     else if (subject === "") {
         input_error.innerHTML = "Subject must be filled out";
-        return false;
     }
     else if (message === "") {
         input_error.innerHTML = "Message must be filled out";
+    }
+
+    if (input_error.innerHTML != ""){
+        input_error.classList.remove("success");
+        input_error.classList.add("form_input_error_visible");
         return false;
     }
 
@@ -98,12 +108,24 @@ function validateForm() {
 }
 
 
-/* input error reset */
-document.getElementById('contact-form').addEventListener('input', function(event) {
-    // Check if the target of the event is an input element
-    if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-        console.log("jh");
-        document.getElementById("form_input_error").innerHTML = "";
+/*************************** INPUT ERROR RESET *************************/
 
-    }
+document.getElementById('contact-form').addEventListener('input', function (event) {
+    resetError(event.target);
 });
+
+document.getElementById('contact-form').addEventListener('click', function (event) {
+    resetError(event.target);
+});
+
+function resetError(target) {
+    var input_error = document.querySelector(".form_input_error");
+
+    // Check if the target of the event is an input element or textarea
+    if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA') {
+        console.log("Resetting error");
+        input_error.innerHTML = "";
+        input_error.classList.remove("form_input_error_visible");
+        input_error.classList.remove("success");
+    }
+}
