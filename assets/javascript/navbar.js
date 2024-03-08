d8c082c427b6b8e39c11d0984021e6eaab2418e4
Giovanni Ruocco <giovanniruocco1@gmail.com>
2024-03-08 18:40:21 +0100

    overlay menu bug fix

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -22,24 +22,16 @@ document.addEventListener("DOMContentLoaded", function () {
     closeBtn.addEventListener("click", function () {
         toggleOverlayNavMenu()
     });
-
-    // Add event listener to each .nav-link inside navMenu
-    overlayNavLinks.forEach(function (navLink) {
-        navLink.addEventListener("click", function () {
-            toggleOverlayNavMenu();
-        });
-    });
 });
 
 
-
 /****************** HANDLE NAV ON SCROLLING *******************/
+
 
 function showBurgerMenu() {
     burgerButton.classList.add('show');
     navMenu.classList.add('nav-links-container-hide');
     navMenu.classList.remove('nav-links-container-show');
-    //navbar.style.backgroundColor = '#131e23'; // Change to your desired background color
     navbar.classList.add('visible');
     navbar.classList.remove('transparent');
 }
@@ -48,7 +40,6 @@ function showNavLinks() {
     burgerButton.classList.remove('show');
     navMenu.classList.add('nav-links-container-show');
     navMenu.classList.remove('nav-links-container-hide');
-    //navbar.style.backgroundColor = 'transparent';
     navbar.classList.remove('visible');
     navbar.classList.add('transparent');
 
@@ -57,7 +48,6 @@ function showNavLinks() {
 document.addEventListener('scroll', function () {
 
     var scrollPosition = window.scrollY;
-    // Adjust this value to determine when the background color should change
     var scrollThreshold = 50;
     var upBtnScrollThreshold = 800;
 
@@ -66,13 +56,11 @@ document.addEventListener('scroll', function () {
 });
 
 
-
 /****************** SCROLL UP BUTTON ******************/
+
 
 document.addEventListener("DOMContentLoaded", function () {
     backUpBtn.addEventListener("click", function () {
-        //document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
-
     });
 });
