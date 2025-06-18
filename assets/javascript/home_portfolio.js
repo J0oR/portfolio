97bd9c772db6354f4968c300a8db4ed2ec35a82b
Giovanni Ruocco <giovanniruocco1@gmail.com>
2025-06-18 09:59:26 +0200

    home redesign done

--- assets/javascript/home_portfolio.js
+++ assets/javascript/home_portfolio.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const projectsArray = [
+        { title: 'Heat Lens', img: './assets/images/projects/heatLens.png' },
+        { title: 'Rooted Recipes', img: './assets/images/projects/rooted.png' },
+        { title: 'Hacker News feed', img: './assets/images/projects/hackerNews.png' },
+        { title: 'Typescript Store demo', img: './assets/images/projects/ts-store-demo.png' },
+        { title: 'Javascript Timer', img: './assets/images/projects/js-timer.png' },
         { title: 'Javascript Calculator', img: './assets/images/calculator.png' },
         { title: 'Javascript Drum Machine', img: './assets/images/drum_machine.png' },
         { title: 'Markdown Previewer', img: './assets/images/markdown.png' },
